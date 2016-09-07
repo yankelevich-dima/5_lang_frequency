52f774f693df3f7a16713fe11ccf4a15eed9291e
Dmitry <yankelevich.dima@gmail.com>
2016-09-07 11:54:47 +0300

    Added lang_frequency

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,28 @@
+import re
+import sys
+from itertools import groupby
 
 
 def load_data(filepath):
-    pass
+    text = ''
+    with open(filepath, 'r') as infile:
+        text = infile.read()
+    return text
 
 
 def get_most_frequent_words(text):
-    pass
+    text = text.lower()
+    # Removing special symbols
+    text = re.sub('\W+', ' ', text)
+    words = list(filter(None, text.split(' ')))
+    words_frequency = {
+        k: len(list(g))/len(words)
+        for k, g in groupby(sorted(words))
+    }
+    return sorted(words_frequency.items(), key=lambda x_y: x_y[1], reverse=True)
 
 
 if __name__ == '__main__':
-    pass
+    filepath = sys.argv[1]
+    for word, frequency in get_most_frequent_words(load_data(filepath))[:10]:
+        print('{} - {}'.format(word, frequency))
