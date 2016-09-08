dcc54067604ab59fb2c55979813565be23889956
yankelevich-dima <yankelevich.dima@gmail.com>
2016-09-08 14:16:41 +0300

    added counting using collections.Counter

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,6 +1,8 @@
 import re
 import sys
-from itertools import groupby
+from collections import Counter
+
+TOP_COUNT = 10
 
 
 def load_data(filepath):
@@ -15,14 +17,11 @@ def get_most_frequent_words(text):
     # Removing special symbols
     text = re.sub('\W+', ' ', text)
     words = list(filter(None, text.split(' ')))
-    words_frequency = {
-        k: len(list(g))/len(words)
-        for k, g in groupby(sorted(words))
-    }
-    return sorted(words_frequency.items(), key=lambda x_y: x_y[1], reverse=True)
+    c = Counter(words).most_common()
+    return list(map(lambda x_y: (x_y[0], x_y[1] / len(words)), c))
 
 
 if __name__ == '__main__':
     filepath = sys.argv[1]
-    for word, frequency in get_most_frequent_words(load_data(filepath))[:10]:
+    for word, frequency in get_most_frequent_words(load_data(filepath))[:TOP_COUNT]:
         print('{} - {}'.format(word, frequency))
